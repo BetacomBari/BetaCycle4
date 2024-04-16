86af578dff599423bc54ce2fbc0d50bbb0855451
AlbertoPanico <panicoalberto99@gmail.com>
2024-04-16 15:03:29 +0200

    encrypt but does rewatch

---
+++ Logic/Authentication/Basic/BasicAuthenticationHandler.cs
@@ -0,0 +1,94 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Options;
+using SqlManager.BLogic;
+using System.Security.Claims;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Text.RegularExpressions;
+using WebAca5CodeFirst.Logic.Autentication;
+using Microsoft.Extensions.Configuration;
+using System.Configuration;
+using Humanizer.Configuration;
+using System.Security.Cryptography.Xml;
+using BetaCycle4.Logic.Authentication.EncryptionWithSha256;
+
+
+
+namespace WebAca5CodeFirst.Logic.Autentication.Basic
+{
+    public class BasicAuthenticationHandler : AuthenticationHandler<AuthenticationSchemeOptions>
+    {
+        //public BasicAuthenticationHandler(IOptionsMonitor<AuthenticationSchemeOptions> options, 
+        //    ILoggerFactory logger, UrlEncoder encoder) : base(options, logger, encoder)
+        //{
+        //}
+        private readonly IConfiguration _configuration;
+        public BasicAuthenticationHandler(IOptionsMonitor<AuthenticationSchemeOptions> options,
+            ILoggerFactory logger, UrlEncoder encoder, ISystemClock clock, IConfiguration configuration) :
+            base(options, logger, encoder, clock)
+        {
+            _configuration = configuration;
+
+        }
+
+        protected override Task<AuthenticateResult> HandleAuthenticateAsync()
+        {
+            Response.Headers.Add("WWW-Authenticate", "Basic");
+
+            if (!Request.Headers.ContainsKey("Authorization"))
+            {
+                return Task.FromResult(AuthenticateResult.Fail("Autorizzazione mancante"));
+            }
+
+            var authorizationHeader = Request.Headers["Authorization"].ToString();
+            var authorHeaderRegex = new Regex("Basic (.*)");
+
+            if (!authorHeaderRegex.IsMatch(authorizationHeader))
+            {
+                return Task.FromResult(AuthenticateResult.Fail("Autorizzazione mancante"));
+            }
+
+            var auth64 = Encoding.UTF8.GetString(
+                Convert.FromBase64String(authorHeaderRegex.
+                Replace(authorizationHeader, "$1")));
+
+            var authArraySplit = auth64.Split(Convert.ToChar(':'), 2);
+            var authUser = authArraySplit[0];
+            var authPassword = authArraySplit.Length > 1 ? authArraySplit[1] : throw new Exception("Password non presente");
+
+            if (string.IsNullOrEmpty(authUser) || string.IsNullOrEmpty(authPassword))
+            {
+                return Task.FromResult(AuthenticateResult.Fail("Username e/o Password non validi"));
+            }
+            else
+            {
+                string passwordFromInput = "";
+                var connecion_string = _configuration.GetConnectionString("ConnectionString");
+                DBUtility dBUtility = new(connecion_string);
+                if (dBUtility.IsDbStatusValid)
+                {
+                    string passwordHashFromDb = dBUtility.getPasswordFromEmail(authUser);
+                    authPassword = EncryptionSHA256.sha256Encrypt(passwordFromInput);
+                    if (passwordHashFromDb == authPassword)
+                    {
+                        // authPassword già con hash? se no va bene come sta fatto
+                        Console.WriteLine("Login effettuato con successo");
+                    }
+                }
+            }
+
+              
+
+
+
+            
+
+            var authenticatedUser = new AuthenticatedUser("BasicAuthentication", true, authArraySplit[0].ToString());
+            var claimsMain = new ClaimsPrincipal(new ClaimsIdentity(authenticatedUser));
+
+            return Task.FromResult(AuthenticateResult.Success(new AuthenticationTicket(claimsMain, Scheme.Name)));
+
+        }
+    }
+}
---
+++ Logic/DbUtility.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using BetaCycle4.Models;
+
+
+namespace SqlManager.BLogic
+{
+    internal class DBUtility
+    {
+        SqlConnection sqlCnn = new();
+        SqlCommand sqlCmd = new();
+        public bool IsDbStatusValid = false;
+
+
+        public DBUtility(string sqlConnectionString)
+        {
+            sqlCnn.ConnectionString = sqlConnectionString;
+            try
+            {
+                using SqlConnection sqlConnection = sqlCnn;
+                sqlConnection.Open(); //lo apro, se va tutto bene reinizializza la connessione 
+                sqlCnn = new SqlConnection(sqlConnection.ConnectionString);
+                IsDbStatusValid = true;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+
+        void checkDbOpen()
+        {
+            if (sqlCnn.State == System.Data.ConnectionState.Closed)
+            {
+                sqlCnn.Open();
+            }
+        }
+
+        void checkDbClose()
+        {
+            if (sqlCnn.State == System.Data.ConnectionState.Closed)
+            {
+                sqlCnn.Close();
+            }
+            sqlCmd.Parameters.Clear();
+        }
+
+        
+        internal string getPasswordFromEmail(string email)
+        {
+            string passwordFromDb = "";
+            try
+            {
+                checkDbOpen();
+                sqlCmd.CommandText = "SELECT password from SalesLT.Customer WHERE SalesLt.Customer.Email = @email";
+                sqlCmd.Parameters.AddWithValue("@email", email);
+                sqlCmd.Connection = sqlCnn;
+
+                using (SqlDataReader sqlReader = sqlCmd.ExecuteReader())
+                {
+                    if (sqlReader.HasRows)
+                    {
+                        while (sqlReader.Read())
+                        {
+                            passwordFromDb = sqlReader["password"].ToString();
+                        }
+                    }
+                }
+                checkDbClose();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            return passwordFromDb;
+        }
+    }
+}
+            
