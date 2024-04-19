06a3e4be6a31fbf60ec64993f11b996d54559fde
AlbertoPanico <panicoalberto99@gmail.com>
2024-04-19 10:43:23 +0200

    add (LocalDb) instead of current PC on context and appsetting

--- Models/AdventureWorksLt2019Context.cs
+++ Models/AdventureWorksLt2019Context.cs
@@ -51,11 +51,11 @@ public partial class AdventureWorksLt2019Context : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-<<<<<<< HEAD
-        => optionsBuilder.UseSqlServer("Data Source=(LocalDb)\SQLEXPRESS;Initial Catalog=AdventureWorksLT2019;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
-=======
+
+        => optionsBuilder.UseSqlServer("Data Source=(LocalDb)\\SQLEXPRESS;Initial Catalog=AdventureWorksLT2019;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+
         //=> optionsBuilder.UseSqlServer("Data Source=DESKTOP-CISCO\\SQLEXPRESS;Initial Catalog=AdventureWorksLT2019;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
->>>>>>> e464053562f87885c50794db43d425c77b8a38dc
+/// e464053562f87885c50794db43d425c77b8a38dc
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
