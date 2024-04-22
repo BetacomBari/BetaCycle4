5002eab26079ce18bf0529fbca385ce9bf34e8df
Francesco Pio Mancino <fmancino51@gmail.com>
2024-04-22 12:56:43 +0200

    work

--- Frontend/src/app/shared/services/httprequest.service.ts
+++ Frontend/src/app/shared/services/httprequest.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Credientals } from '../models/credentials';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttprequestService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  loginPost(credentials: Credientals): Observable<any>
+  {
+    return this.http.post("https://localhost:7177/login", credentials, {observe: 'response'})
+  }
 }
