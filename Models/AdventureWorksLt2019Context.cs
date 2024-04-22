223068cbcd6f65d425e36e992d952131e18fa104
Francesco Pio Mancino <fmancino51@gmail.com>
2024-04-22 11:37:27 +0200

    pull

--- Models/AdventureWorksLt2019Context.cs
+++ Models/AdventureWorksLt2019Context.cs
@@ -51,7 +51,7 @@ public partial class AdventureWorksLt2019Context : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 
-        => optionsBuilder.UseSqlServer("Data Source=DESKTOP-CISCO\\SQLEXPRESS;Initial Catalog=AdventureWorksLT2019;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+        => optionsBuilder.UseSqlServer("Data Source=LAPTOP-2PJI9SPA\\SQLEXPRESS;Initial Catalog=AdventureWorksLT2019;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
 
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
