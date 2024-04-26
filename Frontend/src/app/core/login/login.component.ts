6af20805f5a1cbc5562af22bcb9e3b517e90ced5
AlbertoPanico <panicoalberto99@gmail.com>
2024-04-26 11:59:10 +0200

    ended first tutorial, loginpage done, not the logic

--- Frontend/src/app/core/login/login.component.ts
+++ Frontend/src/app/core/login/login.component.ts
@@ -2,15 +2,21 @@ import { HttpRequest, HttpStatusCode } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { HttprequestService } from '../../shared/services/httprequest.service';
 import { Credientals } from '../../shared/models/credentials';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+  type: string = "password";
+  isText: boolean = false;
+  eyeIcon:string = "fa-eye-slash"
+
   loginCredientals: Credientals = new Credientals()
 
   constructor(private http: HttprequestService) { }
@@ -29,6 +35,12 @@ export class LoginComponent {
         }
       })
     } else (alert("Usurname e password sono obbligatori"))
+  }
+
+  hideShowPassword(){
+    this.isText = !this.isText;
+    this.isText ? this.eyeIcon = "fa-eye" : this.eyeIcon = "fa-eye-slash";
+    this.isText ? this.type="text" : this.type = "password"
   }
 
 }
