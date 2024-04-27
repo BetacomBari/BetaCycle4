f8723ed6f1ef97efb69595398c3121374d72148b
AlbertoPanico <panicoalberto99@gmail.com>
2024-04-27 11:04:40 +0200

    finished second tutorial, login and signup page done

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -2,11 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './core/navbar/navbar.component';
 import { LoginComponent } from './core/login/login.component';
+import { LogoutComponent } from './core/logout/logout.component';
+import { SignupComponent } from './core/signup/signup.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, LoginComponent],
+  imports: [RouterOutlet, NavbarComponent, LoginComponent, LogoutComponent, SignupComponent, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,3 +1,12 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { LogoutComponent } from './core/logout/logout.component';
+import { SignupComponent } from './core/signup/signup.component';
+import { NgModule } from '@angular/core';
+import { LoginComponent } from './core/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"logout", component:LogoutComponent},
+    {path:"signup", component:SignupComponent},
+    {path:"login", component:LoginComponent}
+];
+
