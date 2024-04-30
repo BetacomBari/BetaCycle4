1b589ad034fd0e1dac3f3cc46edbbb93420dcfb3
Francesco Mancino <fmancino51@gmail.com>
2024-04-30 12:37:08 +0200

    Start Register

---
+++ Controllers/RegisterController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace BetaCycle4.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RegisterController : Controller
+    {
+        [HttpPost]
+        public IActionResult Register()
+        {
+
+        }
+    }
+}
