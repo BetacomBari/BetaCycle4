f17c93310ab23d777eff7e15728fee9c2c13d4d8
AlbertoPanico <panicoalberto99@gmail.com>
2024-05-03 16:20:05 +0200

    create skeleton of the register

---
+++ Frontend/src/app/shared/models/customer.ts
@@ -0,0 +1,12 @@
+export class Customer{   
+    
+    firstName: string = ''; 
+    middleName: string = ''; 
+    lastName: string = ''; 
+    email:string = '';
+    password: string = '';
+
+    
+}
+
+
