a006d5e2d5ba1470f1b1f9eaa2861c2930707cf8
AlbertoPanico <panicoalberto99@gmail.com>
2024-05-03 15:41:42 +0200

    customerRegisterController done

--- Controllers/CustomerRegisterController.cs
+++ Controllers/CustomerRegisterController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using BetaCycle4.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BetaCycle4.Controllers
@@ -7,5 +8,28 @@ namespace BetaCycle4.Controllers
     [ApiController]
     public class CustomerRegisterController : ControllerBase
     {
+        public Tuple<Credentials, CustomerNew> splitInfo(CustomerRegister customerRegister)
+        {
+            Credentials credentialToPass = new();
+            CustomerNew customersNewToPass = new();
+
+            credentialToPass.EmailAddress = customerRegister.EmailAddress;
+            credentialToPass.Password = customerRegister.PasswordHash + "|" + customerRegister.PasswordSalt
+
+            customersNewToPass.CustomerId = customerRegister.CustomerId;
+            customersNewToPass.NameStyle = customerRegister.NameStyle;
+            customersNewToPass.Title = customerRegister.Title;
+            customersNewToPass.FirstName = customerRegister.FirstName;
+            customersNewToPass.MiddleName = customerRegister.MiddleName;
+            customersNewToPass.LastName = customerRegister.LastName;
+            customersNewToPass.Suffix = customerRegister.Suffix;
+            customersNewToPass.CompanyName = customerRegister.CompanyName;
+            customersNewToPass.SalesPerson = customerRegister.SalesPerson;
+            customersNewToPass.Phone = customerRegister.Phone;
+            customersNewToPass.Rowguid = customerRegister.Rowguid;
+            customersNewToPass.ModifiedDate = customerRegister.ModifiedDate;
+
+            return Tuple.Create(credentialToPass, customersNewToPass);
+        }      
     }
 }
