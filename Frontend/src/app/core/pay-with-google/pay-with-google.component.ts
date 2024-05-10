9b985c620dfef53c965547ae6d76fcafd7d939b2
AlbertoPanico <panicoalberto99@gmail.com>
2024-05-10 17:54:07 +0200

    why it does not work?

--- Frontend/src/app/core/pay-with-google/pay-with-google.component.ts
+++ Frontend/src/app/core/pay-with-google/pay-with-google.component.ts
@@ -1,3 +1,35 @@
+import { Component } from '@angular/core';
+import { GooglePayButtonModule } from '@google-pay/button-angular';
+import { RouterOutlet } from '@angular/router';
+
+
+@Component({
+  selector: 'app-pay-with-google',
+  standalone: true,
+  imports:  [GooglePayButtonModule, RouterOutlet],
+  templateUrl: './pay-with-google.component.html',
+  styleUrl: './pay-with-google.component.css'
+})
+
+export class PayWithGoogleComponent {
+  title = 'googlepaybtn';
+
+  onLoadPaymentData(event: Event): void {
+    const paymentData = (event as CustomEvent).detail;
+
+    console.log('Data pagamento ricevuti:', paymentData);
+
+    this.processPayment(paymentData);
+  }
+
+  private processPayment(paymentData: any): void {
+    console.log('Elaborazione in corso:', paymentData);
+    console.log('Pagamento OK');
+  }
+}
+
+
+
 // import { Component } from '@angular/core';
 // import { GooglePayButtonModule } from '@google-pay/button-angular';
 // import { RouterOutlet } from '@angular/router';
@@ -57,39 +89,3 @@
 // }
 
 
-
-
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { Component, Injector, inject } from '@angular/core';
-import { GooglePayButtonModule } from '@google-pay/button-angular';
-import { RouterOutlet } from '@angular/router';
-import { NgModule } from '@angular/core';
-
-@Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet,GooglePayButtonModule],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
-})
-
-
-
-
-export class PayWithGoogleComponent {
-  title = 'googlepaybtn';
-
-  onLoadPaymentData(event: Event): void {
-    const paymentData = (event as CustomEvent).detail;
-
-    console.log('Data pagamento ricevuti:', paymentData);
-
-    this.processPayment(paymentData);
-  }
-
-  private processPayment(paymentData: any): void {
-    console.log('Elaborazione in corso:', paymentData);
-    console.log('Pagamento OK');
-  }
-}
