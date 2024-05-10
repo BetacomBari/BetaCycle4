ac533034a7665a66d9df59bfe6b8368516927bf7
Stefano <stefanonitti01@gmail.com>
2024-05-10 17:43:50 +0200

    Jc Code

--- Frontend/src/app/core/pay-with-google/pay-with-google.component.ts
+++ Frontend/src/app/core/pay-with-google/pay-with-google.component.ts
@@ -1,57 +1,87 @@
+// import { Component } from '@angular/core';
+// import { GooglePayButtonModule } from '@google-pay/button-angular';
+// import { RouterOutlet } from '@angular/router';
+
+
+// @Component({
+//   selector: 'app-pay-with-google',
+//   standalone: true,
+//   imports: [ GooglePayButtonModule, RouterOutlet],
+//   templateUrl: './pay-with-google.component.html',
+//   styleUrl: './pay-with-google.component.css'
+// })
+// export class PayWithGoogleComponent {
+
+//   //ngOnInit(){}
+//   constructor(){
+//     console.log("hi");
+//   }
+
+//   buttonWidth = 240
+//   paymentRequest: google.payments.api.PaymentDataRequest = {
+//     apiVersion:2,
+//     apiVersionMinor:0,
+//     allowedPaymentMethods:[
+//       {
+//         type:'CARD',
+//         parameters: {
+//           allowedAuthMethods: ["PAN_ONLY", "CRYPTOGRAM_3DS"],
+//           allowedCardNetworks: ["AMEX", "VISA", "MASTERCARD"]
+//         },
+//         tokenizationSpecification: {
+//           type: "PAYMENT_GATEWAY",
+//           parameters: {
+//             gateway:"example",
+//             gatewayMerchantId:"exampleGatewayMerchantId"
+//           }
+//         }
+//       }
+//     ],
+//     merchantInfo: {
+//       merchantId:"11111",
+//       merchantName: "Demo Merchant"
+//     },
+//     transactionInfo: {
+//       totalPriceStatus:"FINAL",
+//       totalPriceLabel:"TOTAL",
+//       totalPrice:"0.01",
+//       currencyCode:"USD",
+//       countryCode:"US"
+//     }
+//   }
+
+//   onLoadPaymentData(event:any){
+//     console.log(event, ">> Data");
+//   }
+
+// }
+
+
+
+
 import { Component } from '@angular/core';
-import { GooglePayButtonModule } from '@google-pay/button-angular';
 import { RouterOutlet } from '@angular/router';
-
-
+import { GooglePayButtonModule } from '@google-pay/button-angular';
 @Component({
-  selector: 'app-pay-with-google',
+  selector: 'app-root',
   standalone: true,
-  imports: [ GooglePayButtonModule, RouterOutlet],
-  templateUrl: './pay-with-google.component.html',
-  styleUrl: './pay-with-google.component.css'
+  imports: [RouterOutlet,GooglePayButtonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
 })
-export class PayWithGoogleComponent {
+export class AppComponent {
+  title = 'googlepaybtn';
 
-  //ngOnInit(){}
-  constructor(){
-    console.log("hi");
+  onLoadPaymentData(event: Event): void {
+    const paymentData = (event as CustomEvent).detail;
+
+    console.log('Data pagamento ricevuti:', paymentData);
+
+    this.processPayment(paymentData);
   }
 
-  buttonWidth = 240
-  paymentRequest: google.payments.api.PaymentDataRequest = {
-    apiVersion:2,
-    apiVersionMinor:0,
-    allowedPaymentMethods:[
-      {
-        type:'CARD',
-        parameters: {
-          allowedAuthMethods: ["PAN_ONLY", "CRYPTOGRAM_3DS"],
-          allowedCardNetworks: ["AMEX", "VISA", "MASTERCARD"]
-        },
-        tokenizationSpecification: {
-          type: "PAYMENT_GATEWAY",
-          parameters: {
-            gateway:"example",
-            gatewayMerchantId:"exampleGatewayMerchantId"
-          }
-        }
-      }
-    ],
-    merchantInfo: {
-      merchantId:"11111",
-      merchantName: "Demo Merchant"
-    },
-    transactionInfo: {
-      totalPriceStatus:"FINAL",
-      totalPriceLabel:"TOTAL",
-      totalPrice:"0.01",
-      currencyCode:"USD",
-      countryCode:"US"
-    }
+  private processPayment(paymentData: any): void {
+    console.log('Elaborazione in corso:', paymentData);
+    console.log('Pagamento OK');
   }
-
-  onLoadPaymentData(event:any){
-    console.log(event, ">> Data");
-  }
-
 }
