7dc7662ea28fdae6079aa92f7746168478d94222
Francesco Mancino <fmancino51@gmail.com>
2024-05-22 20:20:58 +0200

    work

--- Frontend/src/app/core/signup/signup.component.ts
+++ Frontend/src/app/core/signup/signup.component.ts
@@ -3,30 +3,32 @@ import { RouterModule } from '@angular/router';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { CustomerRegister } from '../../shared/models/CustomerRegister';
 import { HttprequestService } from '../services/httprequest.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 // KANEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [RouterModule, NavbarComponent],
+  imports: [RouterModule, NavbarComponent, CommonModule, FormsModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
 export class SignupComponent {
   type: string = "password";
   isText: boolean = false;
-  eyeIcon:string = "fca-eye-slash"
+  eyeIcon: string = "fca-eye-slash"
   errorMessage: string[] = []
   isRegistered: boolean = false;
   customerRegister: CustomerRegister = new CustomerRegister();
 
-  constructor(private http: HttprequestService){}
+  constructor(private http: HttprequestService) { }
 
-  hideShowPassword(){
+  hideShowPassword() {
     this.isText = !this.isText;
     this.isText ? this.eyeIcon = "fa-eye" : this.eyeIcon = "fa-eye-slash";
-    this.isText ? this.type="text" : this.type = "password"
+    this.isText ? this.type = "text" : this.type = "password"
   }
-  
+
   onSubmit(event: Event) {
     event.preventDefault(); // Previene il comportamento predefinito di submit del form
   }
@@ -90,59 +92,58 @@ export class SignupComponent {
   //! END VERIFY
 
 
-  register(FirstName: HTMLInputElement, MiddleName: HTMLInputElement, LastName: HTMLInputElement, Phone: HTMLInputElement, AddressType: HTMLInputElement, EmailAddress: HTMLInputElement, Password: HTMLInputElement, AddressLine1: HTMLInputElement, AddressLine2: HTMLInputElement, City: HTMLInputElement, StateProvince: HTMLInputElement, CountryRegion: HTMLInputElement, PostalCode: HTMLInputElement, PasswordConfirm:HTMLInputElement):boolean
-  {
-   this.errorMessage = [] 
-    if (!this.verifyLength(FirstName.value.trim(), 50, false)) {      
+  register(FirstName: HTMLInputElement, MiddleName: HTMLInputElement, LastName: HTMLInputElement, Phone: HTMLInputElement, AddressType: HTMLInputElement, EmailAddress: HTMLInputElement, Password: HTMLInputElement, AddressLine1: HTMLInputElement, AddressLine2: HTMLInputElement, City: HTMLInputElement, StateProvince: HTMLInputElement, CountryRegion: HTMLInputElement, PostalCode: HTMLInputElement, PasswordConfirm: HTMLInputElement): boolean {
+    this.errorMessage = []
+    if (!this.verifyLength(FirstName.value.trim(), 50, false)) {
       this.errorMessage.push("Il nome è obbligatorio e non può avere più di 50 caratteri.")
     }
 
-    if (!this.verifyLength(MiddleName.value.trim(), 50, true)) {   
+    if (!this.verifyLength(MiddleName.value.trim(), 50, true)) {
       this.errorMessage.push("Il secondo nome non può avere più di 50 caratteri.")
     }
 
     if (!this.verifyLength(LastName.value.trim(), 50, false)) {
-      this.errorMessage.push("Il cognome è obbligatorio e non può avere più di 50 caratteri.")    
+      this.errorMessage.push("Il cognome è obbligatorio e non può avere più di 50 caratteri.")
     }
 
     if (!this.verifyLength(Phone.value.trim(), 25, false)) {
-      this.errorMessage.push("Il numero di telefono è obbligatorio e non può avere più di 25 caratteri.")    
+      this.errorMessage.push("Il numero di telefono è obbligatorio e non può avere più di 25 caratteri.")
     }
 
     if (!this.verifyLength(AddressType.value.trim(), 50, false)) {
-      this.errorMessage.push("Il tipo di indirizzo è obbligatorio e non può avere più di 50 caratteri.")     
+      this.errorMessage.push("Il tipo di indirizzo è obbligatorio e non può avere più di 50 caratteri.")
     }
 
     if (!this.isValidEmail(EmailAddress.value.trim())) {
-      this.errorMessage.push("L'email è obbligatoria e deve essere simile al formato indicato: 'example@example.com'")     
+      this.errorMessage.push("L'email è obbligatoria e deve essere simile al formato indicato: 'example@example.com'")
     }
 
     if (!this.verifyLength(AddressLine1.value.trim(), 60, false)) {
       this.errorMessage.push("L'indirizzo è obbligatorio e non può avere più di 60 caratteri.")
     }
-    
+
     if (!this.verifyLength(AddressLine2.value.trim(), 60, true)) {
-      this.errorMessage.push("Il secondo indirizzo non può avere più di 60 caratteri.")     
+      this.errorMessage.push("Il secondo indirizzo non può avere più di 60 caratteri.")
     }
-    
+
     if (!this.verifyLength(City.value.trim(), 30, false)) {
-      this.errorMessage.push("La città è obbligatoria e non può avere più di 30 caratteri.")    
+      this.errorMessage.push("La città è obbligatoria e non può avere più di 30 caratteri.")
     }
-    
+
     if (!this.verifyLength(StateProvince.value.trim(), 50, false)) {
-      this.errorMessage.push("La provincia è obbligatoria e non può avere più di 50 caratteri.")     
+      this.errorMessage.push("La provincia è obbligatoria e non può avere più di 50 caratteri.")
     }
-    
+
     if (!this.verifyLength(CountryRegion.value.trim(), 50, false)) {
-      this.errorMessage.push("La regione è obbligatoria e non può avere più di 50 caratteri.")     
+      this.errorMessage.push("La regione è obbligatoria e non può avere più di 50 caratteri.")
     }
-    
+
     if (!this.verifyLength(PostalCode.value.trim(), 15, false)) {
-      this.errorMessage.push("Il CAP è obbligatorio e non può avere più di 15 caratteri.")  
+      this.errorMessage.push("Il CAP è obbligatorio e non può avere più di 15 caratteri.")
     }
-    
+
     if (!this.isValidPassword(Password.value)) {
-      this.errorMessage.push("La password deve avere almeno 8 caratteri, deve contenere almeno un carattere maiuscolo un numero e deve contenere uno dei seguenti caratteri speciali: [!@#$%&?{}|<>].")    
+      this.errorMessage.push("La password deve avere almeno 8 caratteri, deve contenere almeno un carattere maiuscolo un numero e deve contenere uno dei seguenti caratteri speciali: [!@#$%&?{}|<>].")
     }
 
     if (!(Password.value == PasswordConfirm.value)) {
@@ -150,13 +151,13 @@ export class SignupComponent {
     }
 
     if (this.errorMessage.length == 0) {
-      
+
       //! customerNew
       this.customerRegister.FirstName = FirstName.value.trim();
       this.customerRegister.MiddleName = MiddleName.value.trim();
       this.customerRegister.LastName = LastName.value.trim();
       this.customerRegister.Phone = Phone.value.trim();
-  
+
       //! AddressNew
       this.customerRegister.AddressLine1 = AddressLine1.value.trim();
       this.customerRegister.AddressLine2 = AddressLine2.value.trim();
@@ -164,16 +165,38 @@ export class SignupComponent {
       this.customerRegister.StateProvince = StateProvince.value.trim();
       this.customerRegister.CountryRegion = CountryRegion.value.trim();
       this.customerRegister.PostalCode = PostalCode.value.trim();
-  
+
       //! CustomerAddressNew
       this.customerRegister.AddressType = AddressType.value.trim();
-  
+
       //! Credentials
       this.customerRegister.EmailAddress = EmailAddress.value.trim();
       this.customerRegister.Password = Password.value;
-      
+
       this.http.register(this.customerRegister).subscribe({
         next: (customerRegister: CustomerRegister) => {
+
+          //! customerNew
+          FirstName.value = "";
+          MiddleName.value = "";
+          LastName.value = "";
+          Phone.value = "";
+
+          //! AddressNew
+          AddressLine1.value = "";
+          AddressLine2.value = "";
+          City.value = "";
+          StateProvince.value = "";
+          CountryRegion.value = "";
+          PostalCode.value = "";
+
+          //! CustomerAddressNew
+          AddressType.value = "";
+
+          //! Credentials
+          EmailAddress.value = "";
+          Password.value = "";
+          PasswordConfirm.value = "";
           console.log("Register ok");
           return this.isRegistered == true
         },
@@ -183,19 +206,18 @@ export class SignupComponent {
             this.errorMessage.push("L'email inserita è già in uso.")
           }
           this.errorMessage.forEach(element => {
-            console.log(element);     
+            console.log(element);
           });
         }
       })
 
 
-    }else{
-      this.errorMessage = []
+    } else {
       this.errorMessage.forEach(element => {
-        console.log(element);     
+        console.log(element);
       });
-      return this.isRegistered == false  
+      return this.isRegistered == false
     }
-    return this.isRegistered == false  
+    return this.isRegistered == false
   }
 }
