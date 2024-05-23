d67c45d09cf4adfb00aec930ae63788aee710816
Francesco Mancino <fmancino51@gmail.com>
2024-05-23 17:38:52 +0200

    work

--- Frontend/src/app/features/home/home.component.ts
+++ Frontend/src/app/features/home/home.component.ts
@@ -3,17 +3,17 @@ import { NavbarComponent } from '../../core/navbar/navbar.component';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
+import { FooterComponent } from '../../core/footer/footer.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent, FormsModule, CommonModule],
+  imports: [NavbarComponent, FormsModule, CommonModule, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
 
-  
   successMessage: string | null = null;
   
   constructor(private route: ActivatedRoute){}
@@ -22,6 +22,8 @@ export class HomeComponent {
   ngOnInit() {
     this.route.queryParams.subscribe(params => {
       this.successMessage = params['message'];
+      console.log(this.successMessage);
+      
     });
   }
 
