d5a28448d0818a75f44a5411bf48d4e278ca29e1
AlbertoPanico <panicoalberto99@gmail.com>
2024-06-04 16:09:26 +0200

    retreive info from token to be finished

--- Frontend/src/app/core/recommandations/recommandations.component.ts
+++ Frontend/src/app/core/recommandations/recommandations.component.ts
@@ -15,8 +15,16 @@ import { HttprequestService } from '../services/httprequest.service';
 export class RecommandationsComponent {
   product:any = []
   product_id?:number
+  jwtToken: string = "";
+  userId: number = -1;
 
   constructor(private mainhttp: HttprequestService){}
+
+  ngOnInit(){
+    this.getDecodeToken();
+  }
+    
+  
 
   getRecommand(product_id:HTMLInputElement){
     this.product_id = parseInt(product_id.value)
@@ -30,6 +38,55 @@ export class RecommandationsComponent {
         console.log(error);
       }
     })
+  }
+
+
+  decodeBase64Url(str: string): string {
+    // Replace non-URL-safe characters with URL-safe equivalents
+    return decodeURIComponent(atob(str.replace(/-/g, '+').replace(/_/g, '/')));
+  } parseJson(str: string): any {
+    try {
+      return JSON.parse(str);
+    } catch (error) {
+      throw new Error('Invalid JWT payload');
+    }
+  }
+  splitToken(token: string | null): string[] {
+    if (!token || token.length < 3) {
+      throw new Error('Invalid JWT token');
+    }
+    return token.split('.');
+  }
+
+  // FUNZIONI PER LA DECODIFICA DEL TOKEN
+  getDecodeToken() {
+    try {
+      const parts = this.splitToken(this.jwtToken);
+      const decodedHeader = this.decodeBase64Url(parts[0]);
+      const decodedPayload = this.parseJson(this.decodeBase64Url(parts[1]));
+
+      this.mainhttp.getIdFromEmail(decodedPayload.unique_name)
+        .subscribe({
+          next: (response) => {
+            this.userId.push(parseInt(response, 10))
+          },
+          error: (error) => {
+            console.error('Error fetching ID:', error);
+          }
+        });
+
+      // console.log('Header:', decodedHeader);
+      // console.log('Payload:', decodedPayload);
+      // Access specific claims from payload (e.g., username)
+      const username = decodedPayload.sub; // Assuming 'sub' holds username
+      // console.log('Username:', decodedPayload.unique_name);
+    } catch (error) {
+      console.error('Error decoding token:', error);
+    }
+
+
+
+
   }
 
 }
