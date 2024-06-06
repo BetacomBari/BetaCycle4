f82019e84dae56527e499e4dd55ea323104dbbad
AlbertoPanico <panicoalberto99@gmail.com>
2024-06-06 09:44:56 +0200

    commit for pull

--- Controllers/CustomersNewController.cs
+++ Controllers/CustomersNewController.cs
@@ -254,7 +254,7 @@ namespace BetaCycle4.Controllers
                 StatusCode = 200,
                 Message = "Password reset successfully"
             });
-            await _context.PostAsJsonAsync();
+            //await _context.PostAsJsonAsync();
         }
     }
 }
